1cfdc47d4e5cead320dd50135133a427f30aa796
aymenzitouni19 <a.zitouni@esi-sba.dz>
2020-03-29 22:11:25 +0100

    socket system ready

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
 
+const messages = require('./utils/messages')
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -12,16 +14,22 @@ app.use(express.static(path.join(__dirname , 'public')))
 io.on('connection', socket =>{
     console.log('new WS connection')
     //welcome current user
-    socket.emit('message' , 'welcome to socket.io')
+    socket.emit('message' ,messages.formatMessage('chatCordBot','welcome'))
 
     //broadcast when a user connect
-    socket.broadcast.emit('message' , 'a user has joined the chat')
+    socket.broadcast.emit('message' ,messages.formatMessage('ChatCord Bot' , 'a user has joined the cha'))
+
 
 
     //user left the chat
     socket.on('disconnect', ()=>{
-        io.emit('message' , 'a user has left the chat')
+        io.emit('message' ,messages.formatMessage('chatCordBot', 'a user has left the chat '))
     })
+
+    //Listen for ChatMessage
+    socket.on('chatMessage' ,(message)=>{
+        io.emit('message' ,messages.formatMessage('USER2020',message))
+    } )
 })
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,34 @@
 const socket = io()
+const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
 
+//message from server
 socket.on('message' , message =>{
     console.log(message)
+    outputMessage(message)
+
 })
+
+//Message submit 
+chatForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    // Get message text
+    const msg = e.target.elements.msg.value
+
+    // emit message to server
+    socket.emit('chatMessage' , msg)
+    e.target.elements.msg.value = ''
+    e.target.elements.msg.focus()
+
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight
+})
+
+
+function outputMessage(message){
+    var div = document.createElement('div')
+    div.classList.add('message')
+    div.innerHTML = '<p class="meta">'+message.username + '<span>'+message.time+'</span></p><p class="text">'+ message.text +'</p>'
+    document.querySelector('.chat-messages').append(div)
+}
